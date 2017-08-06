6a3761ed62dfff6a8bafd6c1f6a2c5d131ec29dd
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-05 23:12:56 -0400

    gprimelte: Move nfc configs down from common
    Change-Id: Ia4d175bfd97a8fccb1456a9ce0d7919871eb62eb

--- device.mk
+++ device.mk
@@ -11,3 +11,9 @@ LOCAL_PATH := device/samsung/gprimelte
 
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/gprimelte/overlay
+
+# NFC configs
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
+	$(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
+	$(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-sec.conf
