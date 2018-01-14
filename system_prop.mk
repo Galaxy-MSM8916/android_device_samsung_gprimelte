dc4baf28f8013cab6d90b9ad59871cdbfeef4d60
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-01-14 15:33:11 -0500

    gprimelte: Add nfc sysprop to prevent FW download during boot
    Change-Id: Ib423834f3a263c29c70e07f5b0db86947f9544d4

--- system_prop.mk
+++ system_prop.mk
@@ -2,6 +2,10 @@
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.qualcomm.cabl=0
 
+# NFC
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.nfc.sec_hal=true
+
 # RAM
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.low_ram=false \
