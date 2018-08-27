de187e219d2b857d17d1ceebe6913e9da568f5c1
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-08-27 01:42:50 -0400

    gprimelte: Move nfc packages down from gprimelte-common
    Change-Id: Ic7b36def2228f7a405a7689d4ab77385d7abd200

--- device.mk
+++ device.mk
@@ -17,3 +17,20 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
 	$(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
 	$(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-brcm.conf
+
+# Permissions
+PRODUCT_COPY_FILES += \
+	frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+	frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+	frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
+
+# NFC packages
+PRODUCT_PACKAGES += \
+	android.hardware.nfc@1.0-impl \
+	android.hardware.nfc@1.0-service \
+	com.android.nfc_extras \
+	libnfc_nci \
+	libnfc_nci_jni \
+	nfc_nci.msm8916 \
+	NfcNci \
+	Tag
